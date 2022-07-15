4621483bca883678a40767ea64efffa43ea30d4b
zindao <Raymi.Anselmo@gmail.com>
2022-07-15 16:24:03 +0200

    adding mark up into header, front-page and footer to test styles

---
+++ footer.php
@@ -0,0 +1,15 @@
+
+
+    <footer>
+      <!-- Load Boostrap 5 scripts bundle which includes Popper -->
+      <script
+        src="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/js/bootstrap.bundle.min.js"
+        integrity="sha384-ka7Sk0Gln4gmtz2MlQnikT1wXgYsOg+OMhuP+IlRH9sENBO0LRn5q+8nbTov4+1p"
+        crossorigin="anonymous"
+      ></script>
+
+      <!-- Load our custom js scripts bundle -->
+      <script src="dist/js/bundle.js"></script>
+    </footer>
+  </body>
+</html>
