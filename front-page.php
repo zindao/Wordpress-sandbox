dacfab8c226738ae7cd6f9071f6cc11518b68e68
zindao <Raymi.Anselmo@gmail.com>
2022-07-15 16:55:11 +0200

    testing bootstrap classes by adding temp buttons

--- front-page.php
+++ front-page.php
@@ -8,6 +8,14 @@
         <div class="row">
           <div class="col">
             <h1>Hello there</h1>
+
+            <!-- bootstrap class test - button with bootstrap class-->
+            <input type="button" value="Button1" class="btn btn-primary"/>
+            
+            <!-- button without bootstrap class-->
+            <input type="button" value="Button1" />
+
+
           </div>
         </div>
       </div>
