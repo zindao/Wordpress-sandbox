d18dab38f95694f923bfe455be98342ab2c3d232
zindao <Raymi.Anselmo@gmail.com>
2022-07-18 11:07:58 +0200

    merge branch 'dynamic-site-title'

--- header.php
+++ header.php
@@ -7,6 +7,10 @@
     <meta http-equiv="X-UA-Compatible" content="IE=edge" />
     <meta name="viewport" content="width=device-width, initial-scale=1.0" />
 
+
+
+    <!-- Dynamic site title added successfully -->
+
     <?php wp_head(); ?>
     
   </head>
