d9f18aece30af133faca367c756aac291db4aee3
zindao <Raymi.Anselmo@gmail.com>
2022-07-18 10:01:29 +0200

    scripts set up in functions

--- functions.php
+++ functions.php
@@ -13,5 +13,13 @@ function load_styles() {
 add_action('wp_enqueue_scripts', 'load_styles');
 /* Load Styles */
 
+/* Load Scripts */
+function load_js() {
+    wp_register_script('bootstrap-scripts', 'https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/js/bootstrap.bundle.min.js', array('jquery'), null, true );
+    wp_enqueue_script('bootstrap-scripts');
 
-/* commit before changes on new branch for script refactoring */
+    wp_enqueue_script( 'scripts-bundle', get_template_directory_uri() . '/dist/js/bundle.js', array(), null, true );
+    
+  }
+  add_action('wp_enqueue_scripts', 'load_js');
+/* /Load Scripts */
